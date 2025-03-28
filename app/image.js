f0df712d85dd72ef7e378de6801b6baa9ec836c2
devansh1503 <guyawsm@gmail.com>
2025-03-29 00:13:25 +0530

    Added four albums

--- app/image.js
+++ app/image.js
@@ -1,5 +1,18 @@
 export const images = [
     {
+        "album_title":"Scenes From Heaven",
+        "images":[
+            {"url":"https://photos.fife.usercontent.google.com/pw/AP1GczPa95HkR6mLnN9eucbFDCPkorneNv-_ZgUGOiul81tZvKYfWBIpMjV9=w741-h988-s-no-gm?authuser=0"},
+            {"url":"https://photos.fife.usercontent.google.com/pw/AP1GczOgCQk8AeMV6fvD4jC6xV6en64dqzLoCtolabFB1E8-yzuN0bg1DeAJ=w741-h988-s-no-gm?authuser=0"},
+            {"url":"https://photos.fife.usercontent.google.com/pw/AP1GczMuJeWjPFupQTAnuIRv44RXwa3c1Dgtvjk_Y_UXiiS0Xk2Vc97Y7TXa=w741-h988-s-no-gm?authuser=0"},
+            {"url":"https://photos.fife.usercontent.google.com/pw/AP1GczOtn-Jw4JhoSIrW_l6BEmztWLOE46qzx3yxyJBZI51LP_Cm2wa784gE=w741-h988-s-no-gm?authuser=0"},
+            {"url":"https://photos.fife.usercontent.google.com/pw/AP1GczPWcd-XQgqkEp6Fjr_bLORip0LqU_ILkT8m31cm-TlHhOHutH3D328V=w741-h988-s-no-gm?authuser=0"},
+            {"url":"https://photos.fife.usercontent.google.com/pw/AP1GczOs1U3ZvChw_9zMLC4ol6j2RCSOEYndoM4BPYQlSf_Z3huKwkalySJ6=w790-h988-s-no-gm?authuser=0"},
+            {"url":"https://photos.fife.usercontent.google.com/pw/AP1GczPulrgrgYNa9vPz8BCu36TRQPlolEWUGj0ZsmzHeQ1VHYM4a-mLBLFJ=w741-h988-s-no-gm?authuser=0"},
+            {"url":"https://photos.fife.usercontent.google.com/pw/AP1GczPin3MZXnUt0vAvUEVY0Y9NBRk-PyuHOnsp3UW-Hp1eQC1ekZlyiQ9P=w741-h988-s-no-gm?authuser=0"},
+        ]
+    },
+    {
         "album_title":"After Life - Dusk",
         "images":[
             {
